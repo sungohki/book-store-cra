ef9cdb72e91ca750790ef0838f5748b0514d7e62
sungohki <sokim1201@naver.com>
2024-03-23 00:11:46 +0900

    feat: Add Modal UI

--- src/pages/BookDetail.tsx
+++ src/pages/BookDetail.tsx
@@ -11,6 +11,8 @@ import LikeButton from '../components/book/LikeButton';
 import AddToCart from '../components/book/AddToCart';
 import BookReview from '@/components/book/BookReview';
 import { Tab, Tabs } from '@/components/common/Tabs';
+import Modal from '@/components/common/Modal';
+import { useState } from 'react';
 
 const bookInfoList = [
   {
@@ -44,15 +46,24 @@ const bookInfoList = [
 function BookDeatil() {
   const { bookId } = useParams();
   const { book, likeToggle, reviews, addReview } = useBook(bookId);
+  const [isImgOpen, setIsImgOpen] = useState<boolean>(false);
 
   if (!book) return null;
 
   return (
     <BookDeatilStyle>
       <header className="header">
-        <div className="img">
+        <div className="img" onClick={() => setIsImgOpen(true)}>
           <img src={getPicsumImageSrc(book.img)} alt={book.title} />
         </div>
+        <Modal
+          isOpen={isImgOpen}
+          onClose={() => {
+            setIsImgOpen(false);
+          }}
+        >
+          <img src={getPicsumImageSrc(book.img)} alt={book.title} />
+        </Modal>
         <div className="info">
           <Title size="large" color="text">
             {book.title}
